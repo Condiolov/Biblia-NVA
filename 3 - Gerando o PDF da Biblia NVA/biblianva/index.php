7ddc95f008ff21ec8b7ed258e1f88868a7373ef2
Thiago Condé <thiagocondiolov@hotmail.com>
2022-04-24 19:12:04 -0300

    Adcionando mais formas de PDFs da Biblia NVA, tamanho de fonte maior e menor com indeces, necessario rodar no PHP

--- 3 - Gerando o PDF da Biblia NVA/biblianva/index.php
+++ 3 - Gerando o PDF da Biblia NVA/biblianva/index.php
@@ -8,15 +8,17 @@
 # Modif. : Adicionado a capa e os link
 ###################################################################################
 
-//conexão
-$servername = "localhost";
-$username = "usuario";
-$password = "senha";
-$bd = "meuBD";
+# Pego os parametros do banco de dados!!
+if ( file_exists ($_SERVER['DOCUMENT_ROOT'].'/../.meuBD')){
+	$BD_config = file($_SERVER['DOCUMENT_ROOT'].'/../.meuBD', true);
+	preg_match_all('/(.+)=(.+)/m', implode("",$BD_config), $matches);
+	$BD_config=array_combine($matches[1],$matches[2]);
+}
+
 function conectar(){
-	global $servername,$bd,$username, $password,$conn;
+	global $BD_config,$conn;
 	try {
-		$conn = new \PDO("mysql:host=$servername;dbname=$bd;", $username, $password, array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4"));
+		$conn = new \PDO("mysql:host=".$BD_config['servidor'].";dbname=".$BD_config['banco'].";", $BD_config['usuario'], $BD_config['senha'], array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4"));
 		$conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 	}catch(\PDOException $e){
 		echo "Connection failed: " . $e->getMessage();
@@ -27,25 +29,53 @@ function conectar(){
 
 $livro=array("","Gênesis","Êxodo","Levítico","Números","Deuteronômio","Josué","Juízes","Rute","1 Samuel","2 Samuel","1 Reis","2 Reis","1 Crônicas","2 Crônicas","Esdras","Neemias","Ester","Jó","Salmos","Provérbios","Eclesiastes","Cânticos","Isaías","Jeremias","Lamentações","Ezequiel","Daniel","Oséias","Joel","Amós","Obadias","Jonas","Miquéias","Naum","Habacuque","Sofonias","Ageu","Zacarias","Malaquias","Mateus","Marcos","Lucas","João","Atos","Romanos","1 Coríntios","2 Coríntios","Gálatas","Efésios","Filipenses","Colossenses","1 Tessalonicenses","2 Tessalonicenses","1 Timóteo","2 Timóteo","Tito","Filemom","Hebreus","Tiago","1 Pedro","2 Pedro","1 João","2 João","3 João","Judas","Apocalipse");
 
+if (@$_GET['teste']) $teste = (int)@$_GET['teste']+1; else	$teste = 0;
+if (@$_GET['indices']) $mostrar_indices = true; else $mostrar_indices = false;
+if (@$_GET['fonte']) $fonte = (int) @$_GET['fonte']; else $fonte = "18";
+
 
 $conn = conectar();
 $tabela ="bibliaNVA";
 $capitulo = "";
 $controle_capitulo ="";
 $controle_livro = "";
-
-// $mostra_versos = $conn->query("show create table `$tabela`");
-// var_dump( $mostra_versos->fetch(PDO::FETCH_ASSOC));
-// exit;
+$menu="";
 
 include('capa.html');
 
-# criação do menu dos livros
-$menu = "<br>";
-foreach ($livro as $i => $v) {
-	$menu .= "<a href='#ir_".$i."'>$v</a> ";
+echo "<style>
+	body {
+	text-align: justify;
+
+	/*line-height: $fonte px;*/
+	/*height: auto; width: 209mm;*/
+  }
+sup { vertical-align: top; font-size: ". ($fonte/1.5) ."px;}
+
+#biblia { padding: 20px;
+/* margin-top: 50px;*/
+font-family: 'Jura', sans-serif;
+height:auto;
+display:block;
+position: relative;
+font-size: ".$fonte."px;
 }
 
+.c { font-size: ". ($fonte*5) ."px; ". ($fonte*5) ."px; font-weight: bold; float: left; margin: 1px 1px;   line-height: ". ($fonte*3) ."px; }
+</style>
+
+";
+
+# criação do menu dos livros
+if($mostrar_indices){
+	$menu = "<br>";
+	foreach ($livro as $i => $v) {
+		if ($i!=0)
+			$menu .= "<a href='#ir_".$i."'>$v</a> ";
+		if ($i==($teste-1))
+			break;
+	}
+}
 # criação do menu dos capitulos
 $menu_cap="";
 $indices = $conn->query("SELECT MAX(n_capitulo) FROM `$tabela` GROUP BY n_livro");
@@ -54,18 +84,20 @@ $indices=$indices->fetchAll(PDO::FETCH_COLUMN); // pego o numero maximo de versi
 echo "<div id='biblia'>";
 
 $mostra_versos = $conn->query("SELECT * FROM `$tabela`");
-while($resp =$mostra_versos->fetch(PDO::FETCH_ASSOC)){
+while($resp = $mostra_versos->fetch(PDO::FETCH_ASSOC)){
 
 	if ($controle_livro != $resp['n_livro'] ){
 		$menu_cap ="";
-		if ($indices[$resp['n_livro']-1]>3){ // indice apenas para livros com mais de 3 capitulos
-			for ($x = 1; $x <= $indices[$resp['n_livro']-1]; $x++)
-				$menu_cap .= "<a href='#ir_". ($resp['n_livro']) ."_".$x."'>$x</a> ";
 
-			$menu_cap .= "<br>";
-		}
+		if($mostrar_indices)
+			if ($indices[$resp['n_livro']-1]>3){ // indice apenas para livros com mais de 3 capitulos
+				for ($x = 1; $x <= $indices[$resp['n_livro']-1]; $x++)
+					$menu_cap .= "<a href='#ir_". ($resp['n_livro']) ."_".$x."'>$x</a> ";
 
-		$titulo=  "<div style=\"page-break-before: always;\">". $menu ."</div><h1 id='ir_". $resp['n_livro'] ."'>" .$livro[$resp['n_livro']]. "</h1> $menu_cap";
+				$menu_cap .= "<br>";
+			}
+
+		$titulo=  "<div style ='display:block; clear:both; page-break-after:always;''></div>". $menu ."<h1 id='ir_". $resp['n_livro'] ."'>" .$livro[$resp['n_livro']]. "</h1> $menu_cap";
 		$controle_livro = $resp['n_livro'];
 	}else
 		$titulo = "";
@@ -77,13 +109,13 @@ while($resp =$mostra_versos->fetch(PDO::FETCH_ASSOC)){
 		$capitulo =  "";
 
 
-
+	if($teste) if ($resp['n_livro']==$teste) break;
 	echo  $titulo . $capitulo . "<sup>" . $resp['n_versiculo'] . "</sup>" . str_replace(array("Yahweh","Jesus","Deus","Cristo","Senhor"), array(" <b>Yahweh</b>"," <b>Jesus</b>"," <b>Deus</b>"," <b>Cristo</b>"," <b>Senhor</b>"), $resp['versiculo']). " ";
 
 	//  	if ($resp["n_capitulo"]==10){echo "</div>";exit;}
 
 }
 
-echo '<div style="page-break-before: always;"></div><h1>BibliaNVA gerada por BOT da <a href="https://condtec.com.br">condtec.com.br</a></h1>';
+echo '<div style="page-break-before: always;"></div><h1 style="line-height: 70px;">BibliaNVA gerada por BOT da <a href="https://condtec.com.br">condtec.com.br</a></h1>';
 echo "</div>";
 ?>
--- 3 - Gerando o PDF da Biblia NVA/gerar_pdf.sh
+++ 3 - Gerando o PDF da Biblia NVA/gerar_pdf.sh
@@ -7,6 +7,18 @@
 # Info   : 
 ###################################################################################
 # Margem L R T B
-wkhtmltopdf -L 0 -R 0 -T 2 -B 2 --page-size A4 --dpi 600  'localhost/biblianva/'  'bibliaNVA.pdf'
-# pdfunite    'capa.pdf' 'bibliaNVA sem capa.pdf' 'bibliaNVA.pdf'
+link="localhost/_script/ROBONVA/3 - Gerando o PDF da Biblia NVA/biblianva/index.php"
+
+#fonte grande com indice
+ wkhtmltopdf -L 0 -R 0 -T 2 -B 2 --page-size A4 --dpi 600  "$link?teste=0&fonte=18&indices=ok"  'bibliaNVA  GND sem IND.pdf'
+
+#fonte grande com indice
+ wkhtmltopdf -L 0 -R 0 -T 2 -B 2 --page-size A4 --dpi 600  "$link?teste=0&fonte=18"  'bibliaNVA GND com IND.pdf'
+
+#fonte pequena sem indice
+ wkhtmltopdf -L 0 -R 0 -T 2 -B 2 --page-size A4 --dpi 600  "$link?teste=0&fonte=12"  'bibliaNVA PEQ sem IND.pdf'
+
+#fonte pequena sem indice
+ wkhtmltopdf -L 0 -R 0 -T 2 -B 2 --page-size A4 --dpi 600  "$link?teste=0&fonte=12&indices=ok"  'bibliaNVA PEQ com IND.pdf'
+
 
